ba13c94e2353bb473db730849f176655407b66b2
y2-whyto <robertliu042@gmail.com>
2023-04-07 15:55:08 +0800

    First version

---
+++ README.md
@@ -0,0 +1,26 @@
+# Fuchsia Doc 简体中文翻译工具
+
+## 功能
+利用 https://translate.google.com/ 自动按照
+
+```markdown
+<!--
+Original Text
+ -->
+译文
+```
+
+格式进行文档翻译并保存。可以自动跳过代码块。
+
+**注意：本程序仅实现了自动化机器翻译，自动翻译后需要人工核查并调整翻译内容与格式。**
+
+## 使用方法
+
+```shell
+python main.py -i [ipath] -o [opath] -p [proxy]
+```
+- `-i [ipath]`：<b>（必需）</b>指定输入路径，其中 `[ipath]` 为待翻译英文原文件路径
+- `-o [ipath]`：（可选）指定输出路径，其中 `[opath]` 为翻译后文件输出路径。**不指定时覆盖输入文件。**
+- `-p [proxy]`：（可选）指定代理地址，其中 `[proxy]` 为代理地址，形如 `a.b.c.d:e` 或 `protocol://a.b.c.d:e`。**不指定时请确保可以直连 https://translate.google.com/ 。**
+
+亦支持长参数。如需了解请参阅源代码，在此不进行赘述。
---
+++ main.py
@@ -0,0 +1,126 @@
+import os
+import sys
+import getopt
+from time import time
+from googletrans import Translator  # googletrans-py, instead of googletrans
+
+
+def process_trans(translated: str) -> str:
+    translated = translated.replace('＃', '#')
+    if translated.startswith('#'):
+        i = 0
+        while i < len(translated):
+            if translated[i] == '#':
+                i += 1
+            else:
+                if translated[i] != ' ':
+                    translated = translated[:i] + ' ' + translated[i:]
+                break
+    return translated
+
+
+def trans_write(ot_buf: str, tr_buf: str) -> None:
+    if tr_buf[-1] == ' ':
+        tr_buf = tr_buf[:-1]
+    translation = translator.translate(tr_buf, src='en', dest='zh-cn')
+    translated = translation.text
+    if translated == '' and tr_buf != '':
+        print('Translation Exception: Empty Result (Currently at Passage {})'.format(trans_cnt))
+        print('Original text:')
+        print(tr_buf)
+    translated = process_trans(translated)
+    ot_buf = '<!--\n' + ot_buf + ' -->\n'  # add comment mark
+    ot_buf += translated  # append translated text
+    if ot_buf[-1] != '\n':
+        ot_buf += '\n'
+    fo.write(ot_buf + '\n')  # write to fo
+    print('Time elapsed: {:.2f} secs'.format(time() - t1))
+
+
+if __name__ == '__main__':
+    t0 = time()
+    # Deal with arguments
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], '-i:-o:-p:', ['input=', 'output=', 'proxy='])
+    except getopt.GetoptError:
+        print('Arguments Error (Unknown arguments)')
+        exit(0)
+    input_path = ''
+    output_path = ''
+    proxies = None
+    for opt, arg in opts:
+        if opt in ('-i', '--input'):
+            input_path = os.path.expanduser(arg)
+        elif opt in ('-o', '--output'):
+            output_path = os.path.expanduser(arg)
+        elif opt in ('-p', '--proxy'):
+            proxies = {'http': arg, 'https': arg}
+    if input_path == '':
+        print('Argument -i Error')
+        exit(0)
+    if output_path == '':
+        output_path = input_path + '.tmp'
+    if proxies is None:
+        print('Proxy unset. If there\'s empty translation, please check\n'
+              'your connection to https://translate.google.com/')
+
+    translator = Translator(service_urls=['translate.google.com', ], proxies=proxies)
+
+    # Deal with files
+    fi = open(input_path, 'r')
+    fo = open(output_path, 'w')
+    ot_buf = ''  # text to output
+    tr_buf = ''  # to be translated
+    code_area = False
+    trans_cnt = 1
+    code_cnt = 1
+    t1 = time()
+    for line in fi:
+        if line.startswith('```'):
+            if not code_area:
+                code_area = True
+                print('Skipping Code Block {}'.format(code_cnt), end=', ')
+                code_cnt += 1
+                t1 = time()
+            else:
+                ot_buf += line
+                fo.write(ot_buf)
+                ot_buf = ''
+                code_area = False
+                print('Time elapsed: {:.2f} secs'.format(time() - t1))
+                continue
+        if code_area:
+            ot_buf += line
+            continue
+        if line == '\n':
+            if ot_buf == '':
+                fo.write('\n')
+                continue
+            trans_write(ot_buf, tr_buf)
+            trans_cnt += 1
+            ot_buf = ''
+            tr_buf = ''
+            continue
+        if ot_buf == '':
+            print('Translating Passage {}'.format(trans_cnt), end=', ')
+        ot_buf += line
+        # for beginning with special char, restore line break
+        if tr_buf != '' and (not line[0].isalnum()) and (tr_buf[-1] == ' '):
+            tr_buf = tr_buf[:-1] + '\n'
+        tr_buf += line
+        if tr_buf[-1] == '\n':
+            tr_buf = tr_buf[:-1] + ' '
+    if ot_buf != '':  # Deal with remaining text when exiting
+        trans_write(ot_buf, tr_buf)
+        trans_cnt += 1
+        ot_buf = ''
+        tr_buf = ''
+    fi.close()
+    fo.close()
+    if output_path == input_path + '.tmp':
+        os.remove(input_path)
+        os.rename(output_path, input_path)
+    print('Translation completed ({} passages, skipping {} code blocks)'.format(trans_cnt - 1, code_cnt - 1))
+    print('Total time elapsed: {:.5f} secs'.format(time() - t0))
+    print('CAUTIONS: Please check the format of the translated file (especially punctuations),')
+    print('          and correct them manually if needed.')
